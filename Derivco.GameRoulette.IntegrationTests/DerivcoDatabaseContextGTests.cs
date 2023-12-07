4f67a856a281cbb95d66047c77c23c6f06f72313
LindaMotaung <motaung.linda@gmail.com>
2023-12-07 21:38:07 +0200

    feat: Added some integration tests

---
+++ Derivco.GameRoulette.IntegrationTests/DerivcoDatabaseContextGTests.cs
@@ -0,0 +1,52 @@
+using Derivco.GameRoulette.Application.Contracts.Identity;
+using Derivco.GameRoulette.Domain;
+using Derivco.GameRoulette.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Shouldly;
+using System;
+using Xunit;
+
+namespace Derivco.GameRoulette.IntegrationTests
+{
+    /// <summary>
+    /// Mocking the db representation of the db context
+    /// </summary>
+    public class DerivcoDatabaseContextGTests
+    {
+        private DerivcoDatabaseContext _derivcoDatabaseContext;
+        private readonly string _bettorId;
+        private readonly Mock<IUserService> _bettorServiceMock;
+
+        public DerivcoDatabaseContextGTests() 
+        {
+            //Creating the database in memory whenever that db context is invoked
+            var dbOptions = new DbContextOptionsBuilder<DerivcoDatabaseContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options; //Get a random database with a new Guid each time
+            _bettorId = "00000000-0000-0000-0000-000000000000";
+            _bettorServiceMock = new Mock<IUserService>();
+            _bettorServiceMock.Setup(m => m.BettorId).Returns(_bettorId);
+            _derivcoDatabaseContext = new DerivcoDatabaseContext(dbOptions, _bettorServiceMock.Object);
+        }
+
+        /// <summary>
+        /// Scenario to test for when a bet is created
+        /// </summary>
+        [Fact]
+        public async void Save_SetDateCreatedValue()
+        {
+            // Arrange
+            var betType = new BetType
+            {
+                Id = 1,
+                Name = "Inside bets"
+            };
+
+            // Act
+            await _derivcoDatabaseContext.BetTypes.AddAsync(betType);
+            await _derivcoDatabaseContext.SaveChangesAsync();
+
+            // Assert
+            betType.DateCreated.ShouldNotBeNull();
+        }
+    }
+}
--- SQL - Stored Procedure/README.txt
+++ SQL - Stored Procedure/README.txt
@@ -11,9 +11,10 @@
  > Considering that the data set was quite reletively small, there were no performance bottlenecks to consider
  > The tables in the Northwind database have appropriate clustered indexing 
 
-- The Web API component of the project I created with ASP.Net Core using Clean Architecture, CQRS, SOLID Principles and the Mediator pattern. My choice for this design was as follows:
+- The Web API component of the project I created with ASP.Net Core using Clean Architecture, CQRS, SOLID Principles and the Mediator pattern.My choice for this design was as follows:
   > I wanted to design for scalability. In the event that the business wants to enhance the requirement to not only allow russian roulette but also black jack for instance. Hence the Bet Type feature
   > Implemeneted detailed seperation of concern. The API knows that it needs to do something. How it's supposed to do it is not concerned with that. That abstraction is facilitated through the mediator pattern. This makes the application truly closed for modification and open for extension. 
   > Made use of CQRS design pattern to control how the system should seperate commands that manage an object's state compared to queries that are responsible for retrieving data. The models used for writing data (placing a bet) differ than the models used for reading data (e.g a bettor's allocated bets)
   > Clean architecture ensures that each layer in the system has a single responsibility and should only have one reason to change i.e changing the email service (infrastructure layer) should not affect the bet service (applicaiton layer)
+  > I used Entity Framework over Dapper as my ORM of choice as it is the one I am most comfortable with
 
